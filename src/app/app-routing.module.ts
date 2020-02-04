c10933257ce55cca39c4c653ee5d8341e5c81d70
emu <bsse0927@iit.du.ac.bd>
2020-02-04 13:15:39 +0600

    upto routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {RouterModule , Routes } from '@angular/router';
 
+import { CreateEmployeeComponent } from './employee/create-employee.component';
+import { ListEmployeesComponent } from './employee/list-employees.component';
 
-const routes: Routes = [];
+// const routes: Routes = [];
+const appRoutes: Routes = [
+  { path: 'list', component: ListEmployeesComponent },
+  { path: 'create', component: CreateEmployeeComponent },
+  { path: '', redirectTo: '/list', pathMatch: 'full' }
+];
+
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(appRoutes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
