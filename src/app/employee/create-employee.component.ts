c13efeffd687a41bcdb16878d2eb4f02635ebdce
emu <bsse0927@iit.du.ac.bd>
2020-02-06 17:28:14 +0600

    upto dynamic

--- src/app/employee/create-employee.component.ts
+++ src/app/employee/create-employee.component.ts
@@ -19,6 +19,9 @@ export class CreateEmployeeComponent implements OnInit {
     'email': {
       'required': 'Email is required.'
     },
+    'phone': {
+      'required': 'Phone is required.'
+    },
     'skillName': {
       'required': 'Skill Name is required.',
     },
@@ -34,6 +37,7 @@ export class CreateEmployeeComponent implements OnInit {
   formErrors = {
     'fullName': '',
     'email': '',
+    'phone': '',
     'skillName': '',
     'experienceInYears': '',
     'proficiency': ''
@@ -44,18 +48,38 @@ export class CreateEmployeeComponent implements OnInit {
   ngOnInit() {
     this.employeeForm = this.fb.group({
       fullName: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(10)]],
-      email: ['', Validators.required ],
+      contactpreference: ['email'],
+      email: ['', Validators.required],
+      phone: [''],
       skills: this.fb.group({
-        skillName: ['', Validators.required ],
+        skillName: ['', Validators.required],
         experienceInYears: ['', Validators.required],
-        proficiency: ['', Validators.required ]
+        proficiency: ['', Validators.required]
       })
     });
 
-    this.employeeForm.valueChanges.subscribe((data)=> {
+    this.employeeForm.get('contactpreference')
+      .valueChanges.subscribe((data: string) => {
+        this.onContactPreferenceChange(data);
+      });
+
+    this.employeeForm.valueChanges.subscribe((data) => {
       this.logValidationErrors(this.employeeForm);
     });
   }
+
+  onContactPreferenceChange(selectedValue: string) {
+    const phoneControl = this.employeeForm.get('phone');
+    if (selectedValue === 'phone') {
+      phoneControl.setValidators(Validators.required);
+    } else {
+      phoneControl.clearValidators();
+
+    }
+    phoneControl.updateValueAndValidity();
+
+  }
+
   logValidationErrors(group: FormGroup = this.employeeForm): void {
     Object.keys(group.controls).forEach((key: string) => {
       const abstractControl = group.get(key);
@@ -79,7 +103,7 @@ export class CreateEmployeeComponent implements OnInit {
 
 
   onLoadDataClick(): void {
-    this.logValidationErrors(this.employeeForm);
-    console.log(this.formErrors);
+    // this.logValidationErrors(this.employeeForm);
+    // console.log(this.formErrors);
   }
 }
