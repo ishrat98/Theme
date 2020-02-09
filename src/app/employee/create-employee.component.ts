9adfbd592e29ef2788276a0105b59dfcce6798e9
emu <bsse0927@iit.du.ac.bd>
2020-02-09 11:21:54 +0600

    upto custom validator

--- src/app/employee/create-employee.component.ts
+++ src/app/employee/create-employee.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators , AbstractControl } from '@angular/forms';
 
 @Component({
   selector: 'app-create-employee',
