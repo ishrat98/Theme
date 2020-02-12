4120e3ed7e9c7ad63c4af2571fc85c0ef1e1fc18
emu <bsse0927@iit.du.ac.bd>
2020-02-12 11:57:53 +0600

    upto home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,18 @@ import {RouterModule , Routes } from '@angular/router';
 
 import { CreateEmployeeComponent } from './employee/create-employee.component';
 import { ListEmployeesComponent } from './employee/list-employees.component';
+import { HomeComponent } from './home.component';
+import { PageNotFoundComponent } from './page-not-found.component';
 
 // const routes: Routes = [];
 const appRoutes: Routes = [
+  { path: 'home', component: HomeComponent },
   { path: 'list', component: ListEmployeesComponent },
   { path: 'create', component: CreateEmployeeComponent },
   { path: 'edit/:id', component: CreateEmployeeComponent },
-  { path: '', redirectTo: '/list', pathMatch: 'full' }
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  // wild card route
+  { path: '**', component: PageNotFoundComponent }
 ];
 
 
