97ffd1ee5ad845771dbd03c31f01a4533b7b0c95
emu <bsse0927@iit.du.ac.bd>
2020-02-12 10:50:19 +0600

    upto put method

--- src/app/employee/create-employee.component.ts
+++ src/app/employee/create-employee.component.ts
@@ -5,6 +5,7 @@ import { ActivatedRoute } from '@angular/router';
 import { EmployeeService } from './employee.service';
 import { IEmployee } from './IEmployee';
 import { ISkill } from './ISkill';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-create-employee',
@@ -13,7 +14,8 @@ import { ISkill } from './ISkill';
 })
 export class CreateEmployeeComponent implements OnInit {
   employeeForm: FormGroup;
-  fullNameLength = 0;
+  employee: IEmployee;
+  // fullNameLength = 0;
   // This object contains all the validation messages for this form
   validationMessages = {
     'fullName': {
@@ -44,7 +46,8 @@ export class CreateEmployeeComponent implements OnInit {
 
   constructor(private fb: FormBuilder,
               private route: ActivatedRoute,
-              private employeeService: EmployeeService) { }
+              private employeeService: EmployeeService,
+              private router: Router) { }
 
   ngOnInit() {
     this.employeeForm = this.fb.group({
@@ -82,7 +85,12 @@ export class CreateEmployeeComponent implements OnInit {
   getEmployee(id: number) {
     this.employeeService.getEmployee(id)
       .subscribe(
-        (employee: IEmployee) => this.editEmployee(employee),
+        (employee: IEmployee) => {
+          // Store the employee object returned by the
+          // REST API in the employee property
+          this.employee = employee;
+          this.editEmployee(employee);
+        },
         (err: any) => console.log(err)
       );
   }
@@ -165,6 +173,7 @@ export class CreateEmployeeComponent implements OnInit {
   }
 
 
+
   onLoadDataClick(): void {
 
     const formArray1 = this.fb.array([
@@ -186,15 +195,20 @@ export class CreateEmployeeComponent implements OnInit {
 
 
   onSubmit(): void {
-
-    console.log(this.employeeForm.touched);
-    console.log(this.employeeForm.value);
-
-    console.log(this.employeeForm.controls.touched);
-    console.log(this.employeeForm.get('fullName').value);
+    this.mapFormValuesToEmployeeModel();
+    this.employeeService.updateEmployee(this.employee).subscribe(
+      () => this.router.navigate(['list']),
+      (err: any) => console.log(err)
+    );
+  }
+  mapFormValuesToEmployeeModel() {
+    this.employee.fullName = this.employeeForm.value.fullName;
+    this.employee.contactPreference = this.employeeForm.value.contactPreference;
+    this.employee.email = this.employeeForm.value.emailGroup.email;
+    this.employee.phone = this.employeeForm.value.phone;
+    this.employee.skills = this.employeeForm.value.skills;
   }
 }
-
 function matchEmail(group: AbstractControl): { [key: string]: any } | null {
   const emailControl = group.get('email');
   const confirmEmailControl = group.get('confirmEmail');
